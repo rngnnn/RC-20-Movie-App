20a64d25eab29830710d6a5598fb5602506ffcce
rngnnn <rngnnnclk@gmail.com>
2024-08-21 11:07:36 +0200

    AuthContext

--- movie-app-inclass/src/App.js
+++ movie-app-inclass/src/App.js
@@ -3,12 +3,17 @@ import React from "react";
 import Register from "./pages/Register";
 import AppRouter from "./router/AppRouter";
 import { ToastContainer } from "react-toastify";
+import AuthContext from "./context/AuthContext";
 
 const App = () => {
   return (
     <div className="dark:bg-[#23242a] min-h-screen">
+      <AuthContext>
     <AppRouter/>
     <ToastContainer/>
+</AuthContext>
+
+
     </div>
   );
 };
--- movie-app-inclass/src/context/AuthContext.js
+++ movie-app-inclass/src/context/AuthContext.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import React, { createContext } from 'react'
 
-const AuthContext = () => {
+
+//!context alanı açtık
+export const AuthContextt=createContext()
+
+
+
+const AuthContext = ({children}) => {
+
+//!register için (sitede zincir yapılı fetch işlemi var biz burada async await i tercih ettik)
+
+const createUser=async()=>{
+
+createUserWithEmailAndPassword(auth, email, password);
+
+}
+
+
+
+
   return (
-    <div>AuthContext</div>
+    <AuthContextt.Provider value={{}}>{children}</AuthContextt.Provider>
   )
 }
 
